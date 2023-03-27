e0978c28eb4c153a11a5b4dc39df58951b6a139e
soumicshahriar <soumicshahriar1@gmail.com>
2023-03-27 22:47:43 +0600

    product save

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,39 +5,56 @@ import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
- const [products, setProducts]=useState([]);
- const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
- useEffect(() => {
-   fetch('products.json')
-   .then(res=>res.json())
-   .then(data=>setProducts(data))
-   
- } , []);
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
 
- useEffect( () => {
-  const storedCart = getShoppingCart();
- } , [])
+  }, []);
 
- const handleAddToCart=(product)=>{
-  // console.log(product)
-  const newCart = [...cart, product ]
-  setCart(newCart)
-  addToDb(product.id)
- }
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const saveCart = [];
+    // step 1: get id from the added object
+    for (const id in storedCart) {
+      // get the product by using id
+      const addedProduct = products.find(product => product.id === id)
+      if (addedProduct) {
+        // step 3: get quantity of the product
+        const quantity = storedCart[id]
+        addedProduct.quantity = quantity;
 
- return (
-  <div className='shop-container'>
-   <div className="products-container">
-    {
-     products.map(product =><Product product={product} key={product.id} handleAddToCart={handleAddToCart} ></Product>)
+        // step 4: add the added product to the saveCart
+        saveCart.push(addedProduct)
+        console.log(addedProduct)
+      }
     }
-   </div>
-   <div className="card-container">
-    <Cart cart={cart}></Cart>
-   </div>
-  </div>
- );
+    // set the cart
+    setCart(saveCart)
+  }, [products])
+
+  const handleAddToCart = (product) => {
+    // console.log(product)
+    const newCart = [...cart, product]
+    setCart(newCart)
+    addToDb(product.id)
+  }
+
+  return (
+    <div className='shop-container'>
+      <div className="products-container">
+        {
+          products.map(product => <Product product={product} key={product.id} handleAddToCart={handleAddToCart} ></Product>)
+        }
+      </div>
+      <div className="card-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
